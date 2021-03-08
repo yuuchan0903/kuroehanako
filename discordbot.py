7a99ef2ea7c43d1e4b9f65257628273333577243
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2021-03-09 01:05:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,31 +6,20 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('ぽーん')
-
-anync def reply(message):
-    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-    await message.channel.send(reply) # 返信メッセージを送信
-    
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-        
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    # コマンドに対応するデータを取得して表示
-    print(get_data(message))
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
