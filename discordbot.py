36161fe522f0ed9b0fb0165251c3acf17c6f0930
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2021-03-08 17:54:45 +0900

    ぽーーん

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def 'OK?ロゼッタ'(ctx):
+    await ctx.send('ぽーーん')
 
 
 bot.run(token)
