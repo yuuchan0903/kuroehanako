fb39c5e312db3c9a25109d7bb9dac46ea44522fe
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2021-03-09 02:24:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,16 +16,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong') 
-    
-# 返信する非同期関数を定義
-async def reply(message):
-    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-    await message.channel.send(reply) # 返信メッセージを送信
-
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行        
+         
     
 bot.run(token)
