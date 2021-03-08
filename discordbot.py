abb5e141327c40537a50743fd989934751c911ec
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2021-03-09 01:51:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,26 +13,9 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
     
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-
-# 返信する非同期関数を定義
-async def reply(message):
-    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-    await message.channel.send(reply) # 返信メッセージを送信
-
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行        
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')       
         
     
 bot.run(token)
