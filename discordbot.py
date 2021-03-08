b6c812da21926951c4fc00cf687ba03201262dbf
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2021-03-09 01:44:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,8 +11,22 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+    
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
