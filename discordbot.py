8d77c3c55367136375b02f90d078195c085fd6b1
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2021-03-09 00:49:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,6 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('ぽーん')
 
-@bot.comand()
 anync def reply(message):
     reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
     await message.channel.send(reply) # 返信メッセージを送信
