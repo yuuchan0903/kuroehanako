17c3dca062571d169ffa955caa8ab3652d80e0dc
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2021-03-09 22:48:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,29 +5,18 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-    
+
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong') 
-    
-    
-@bot.command()
-# 返信する非同期関数を定義
-async def reply(message):
-    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-    await message.channel.send(reply) # 返信メッセージを送信
+    await ctx.send('pong')
 
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
 
-    
 bot.run(token)
+© 2021 GitHub, Inc.
