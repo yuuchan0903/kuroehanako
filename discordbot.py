f75f94e58d0a57f5c6f6f1cd08c7d5aabfb33acb
yuuchan0903 <79980453+yuuchan0903@users.noreply.github.com>
2022-01-18 22:34:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,22 +1,12 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
+import gspread
+client = discord.Client()
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['ODE2NzA4MTM1OTIxMTIzMzk5.YD-4uQ.yEM7IVxIxsOzI9ZQNPcXN1qf_gE']
+TOKEN = 'OTMyNDU5MzM3MzEyNTMwNDYz.YeTSbg.wJlgpx-3-wf_cQ1HRdWNuDapvNI'
 
+@client.event
+async def on_ready():
+    print("on_ready")
+    print(discord.__version__)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
-© 2021 GitHub, Inc.
+client.run(TOKEN)
